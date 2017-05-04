7e287da57a6302a619edee3b23719434714016ee
BuddleM <1870408473@qq.com>
2017-05-04 17:50:31 +0800

    完成推荐

--- test.py
+++ test.py
@@ -42,8 +42,36 @@ def computeNearestNeighbor(username,users):
 	distances.sort()
 	return distances
 
-print(computeNearestNeighbor("Hailey",users))
+#print(computeNearestNeighbor("Hailey",users))
 """ 
 [(2.0, 'Veronica'), (4.0, 'Chan'), (4.0, 'Sam'), (4.5, 'Dan'), (5.0, 'Angelica'), 
 (5.5, 'Bill'), (7.5, 'Jordyn')]
 """
+
+#开始做推荐
+def recommend(username,users):
+	nearest = computeNearestNeighbor(username,users)[0][1] # 找到距离最近的用户
+	recommendations = []
+	neighborRatings = users[nearest] # 取得距离最近的用户的所有评价数据
+	userRatings = users[username] # 当前用户的所有评价数据
+	for artist in neighborRatings:
+		if not artist in userRatings: #找到最近用户评价过而自己没有评价的乐队
+			recommendations.append((artist,neighborRatings[artist]))
+	return sorted(recommendations,
+				key=lambda artistTuple:artistTuple[1],                                    #/**************/
+				reverse=True) #按评分进行排序
+print(recommend("Hailey",users)) 
+''' 
+[('Phoenix', 4.0), ('Blues Traveler', 3.0), ('Slightly Stoopid', 2.5)]
+'''
+
+
+#计算闵可夫斯基距离
+def minkowski(rating1,rating2,r):
+	distance = 0
+	for key in rating1:
+		if key in rating2:
+			distance += pow(abs(rating1[key] - rating2[key]),r)
+	return pow(distance,1.0/r)
+distance = minkowski(users[user],users[username],2) #使用欧几里德距离 
+
