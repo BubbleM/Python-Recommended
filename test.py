3a95f9f84af531f1b3a60eab651039301afb9700
BuddleM <1870408473@qq.com>
2017-05-06 23:44:01 +0800

    update readme

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import codecs #导入自然语言编码转换包
 from math import sqrt
+
 users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0,
 						 "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
          "Bill":     {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, 
@@ -17,34 +17,29 @@ users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones":
          "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5,
          		 		"The Strokes": 3.0}
         } 
-class recommender:
-   def __init__(self,data,k=1,metric='pearson',n=5):
-      """ 初始化推荐模块
-      data 训练数据
-      k    k邻近算法中的值
-      metric  使用何种距离计算方式
-      n  推荐结果的数量
-      """
-      self.k = k
-      self.n = n
-      self.username2id = {}
-      self.userid2name = {}
-      self.productid2name = {}
-      #将距离计算方式保存下来
-      self.metric = metric
-      if self.metric == 'pearson':
-         self.fn = self.pearson
-      #
-      # 如果data是一个字典类型，则保存下来，否则忽略
-      #
-      if type(data).__name__ == 'dict':
-         self.data = data
+def pearson(rating1,rating2):
+   sum_xy = 0
+   sum_x = 0
+   sum_y = 0
+   sum_x2 = 0
+   sum_y2 = 0
+   n = 0
+   for key in rating1:
+      if key in rating2:
+         n += 1
+         x = rating1[key]
+         y = rating2[key]
+         sum_xy += x * y
+         sum_x += x
+         sum_y += y
+         sum_x2 += pow(x,2)
+         sum_y2 += pow(y,2)
+   #计算得分
+   denominator = sqrt(sum_x2-pow(sum_x,2)/n) * sqrt(sum_y2 - pow(sum_y,2)/n)
+   if denominator == 0:
+      return 0
+   else:
+      return (sum_xy-(sum_x * sum_y)/n)/denominator
 
-   def convertProductID2name(self,id):
-      '''通过产品ID获取名称'''
-      
-
-
-
-
-      
+print(pearson(users["Angelica"],users["Bill"]))
+#-0.9040534990682699
