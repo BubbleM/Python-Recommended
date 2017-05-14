14ed0a444b7ec1980b8d904fccd11768e0d3da8e
BuddleM <1870408473@qq.com>
2017-05-14 17:51:09 +0800

    重构电影推荐函数 成功使用曼哈顿进行推荐

--- movieRecommend.py
+++ movieRecommend.py
@@ -1,73 +1,88 @@
 import csv
 
 fileurl = "./BX-Dump/Movie_Ratings.csv"
-users = {}
-bands = {}
 with open(fileurl) as f:
     reader = csv.reader(f)
     header_row = next(reader) # 电影数组
-    del header_row[0] # 删除电影中第一个空字段
-
+    # del header_row[0] # 删除电影中第一个空字段
     rows = [row for row in reader]
-# print(rows[24][25])
+# print(header_row)
+# print(rows[2])
 
-for i in range(0,25):
-  for j in range(1,26):
-    if rows[i][j] == '':
-      rows[i][j] = 0
+# 将分数从字符串转换为数值
+for row in range(0,25):
+  for col in range(1,26):
+    if rows[row][col] != '':
+      rows[row][col] = int(rows[row][col])
     else:
-      rows[i][j] = int(rows[i][j])
+      rows[row][col] = 0
+# print(rows)
 
-for i in range(0,25):
-  for j in range(1,26):
-    users[rows[i][0]] = bands
-    bands[header_row[i]] = rows[i][j]
-# print(users['Jaws'])
+# 查找到所有用户并将其保存在一个用户列表users中
+users = []
+def user():
+  for i in range(0,25):
+    users.append(rows[i][0])
+    # print(rows[i][0])
+user()
+# print(users)
 
-#计算曼哈顿距离
-def manhattan(rating1,rating2):
-  distance = 0;
-  for key in rating1:
-    if key in rating2: # 找到共同评分的乐队
-      distance += abs(rating1[key] - rating2[key])
+
+# 根据用户名查找   并返回该用户所在的行的数据
+def findUser(username):
+  for row in range(0,25):
+      if rows[row][0] == username:
+        return  rows[row]
+# print(findUser('Old School'))
+
+# 曼哈顿实现计算两个用户之间的距离 并返回距离
+def manhattan(username1,username2):
+  rating1 = findUser(username1)
+  rating2 = findUser(username2)
+  distance = 0
+  for i in range(1,26):
+    if rating1[i] != 0 and rating2[i] != 0:
+      distance += abs(rating1[i] - rating2[i])
   return distance
-# print(manhattan(users["Avatar"],users["Kazaam"])) 
-# 输出2.0 
+# print(manhattan("Jaws","Gladiator"))
 
 
-#编写函数来找到距离最近的用户（返回一个用户列表，按距离排序）
+# 编写函数来找到距离最近的用户（返回一个用户列表，按距离排序）
 def computeNearestNeighbor(username,users):
-  """计算所有用户至unername用户的距离，倒序排列并返回结果列表"""
   distances = []
   for user in users:
     if user != username:
-      distance = manhattan(users[user],users[username])
+      distance = manhattan(username,user)
       distances.append((distance,user))
-  # 按距离排序——距离近的排在前面
   distances.sort()
   return distances
+# print(computeNearestNeighbor("Jaws",users))
 
-#print(computeNearestNeighbor("Hailey",users))
-""" 
-[(2.0, 'Veronica'), (4.0, 'Chan'), (4.0, 'Sam'), (4.5, 'Dan'), (5.0, 'Angelica'), 
-(5.5, 'Bill'), (7.5, 'Jordyn')]
-"""
-
-#开始做推荐
+# 开始做推荐啦
 def recommend(username,users):
-  nearest = computeNearestNeighbor(username,users)[0][1] # 找到距离最近的用户
+  # 找到距离最近的用户
+  nearest = computeNearestNeighbor(username,users)[0][1] 
   recommendations = []
-  neighborRatings = users[nearest] # 取得距离最近的用户的所有评价数据
-  userRatings = users[username] # 当前用户的所有评价数据
-  for artist in neighborRatings:
-    if not artist in userRatings: #找到最近用户评价过而自己没有评价的乐队
-      recommendations.append((artist,neighborRatings[artist]))
+  neighborRatings = findUser(nearest) # 取得距离最近的用户的数据
+  userRatings = findUser(username) # 取得当前用户的数据
+  for i in range(1,26):
+    if neighborRatings[i] != 0 and userRatings[i] == 0:
+      recommendations.append((header_row[i],neighborRatings[i]))
   return sorted(recommendations,
-        key=lambda artistTuple:artistTuple[1],                                    #/**************/
-        reverse=True) #按评分进行排序
-# print(recommend("Hailey",users)) 
-''' 
-[('Phoenix', 4.0), ('Blues Traveler', 3.0), ('Slightly Stoopid', 2.5)]
-'''
+        key=lambda artistTuple:artistTuple[1],
+        reverse=True)
 
-print(recommend('Star Wars',users))
+
+# 输出测试
+
+# print(recommend("Old School",users))
+# 输出[('Stephen', 5), ('aaron', 4), ('Josh', 3)]
+
+# print(recommend("Jaws",users))
+# 输出[('Patrick C', 1), ('Zak', 1)]
+
+# print(recommend("Village",users))
+# 输出[('aaron', 3), ('Jonathan', 2), ('Amy', 2), ('Patrick C', 1), ('Gary', 1)]
+
+print(recommend("Alien",users))
+# 输出[('Heather', 4), ('vanessa', 4), ('greg', 4), ('ben', 4), ('Jonathan', 4), ('Valerie', 4), ('Zak', 3)]
